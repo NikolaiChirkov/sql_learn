cdba4d50654f443b77184b0dfb93ed91972116e0
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-08-28 21:17:26 +0300

    Constraints

--- simple_DB.sql
+++ simple_DB.sql
@@ -1,14 +1,18 @@
 CREATE TABLE student (
-    student_id INT ,
-    name VARCHAR(20),
-    major VARCHAR(20),
+    student_id INT AUTO_INCREMENT, /* AUTO_INCREMENT for auto increment value */
+    name VARCHAR(20), /* NOT NULL for not null */
+    major VARCHAR(20), /* DEFAULT 'undecided' for default 'undecided'*/ /* UNIQUE for unique */
     PRIMARY KEY(student_id)
 );
 
 SELECT * FROM student;
 
-INSERT INTO student VALUES(1, 'Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
 
-INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
 
-INSERT INTO student(student_id, name) VALUES(3, 'Claire');
+INSERT INTO student(name, major) VALUES('Claire', 'Chemistry');
+
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+
+INSERT INTO student(name, major) VALUES('Mike', 'Computer Science');
